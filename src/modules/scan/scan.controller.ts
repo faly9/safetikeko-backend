c00a45ad00075dba56764c2c9ed9fd4779e1d570
faly9 <randriantsoafaly8@gmail.com>
2026-05-29 01:16:25 +0300

    scan

--- src/modules/scan/scan.controller.ts
+++ src/modules/scan/scan.controller.ts
@@ -1,18 +1,17 @@
-// scan/scan.controller.ts
 import {
   Controller,
   Post,
+  Get,
   Body,
   HttpCode,
   HttpStatus,
-  // UseGuards,
+  Param,
+  ParseIntPipe,
 } from '@nestjs/common'
 import { ScanService } from './scan.service'
 import { CreateScanDto } from './dto/create-scan.dto'
-import { SCANQRCODE } from 'src/routes/user.routes'
+import { SCANQRCODE, GETHISTOSCAN } from 'src/routes/user.routes'
 
-// import { AuthGuard } from '@nestjs/passport'
-// UseGuards(AuthGuard)
 @Controller('scan')
 export class ScanController {
   constructor(private readonly scanService: ScanService) {}
@@ -21,5 +20,10 @@ export class ScanController {
   @HttpCode(HttpStatus.OK)
   async scanner(@Body() dto: CreateScanDto) {
     return this.scanService.scanQRCode(dto)
+  }
+
+  @Get(GETHISTOSCAN)
+  async getHistorique(@Param('agent_id', ParseIntPipe) agent_id: number) {
+    return this.scanService.getHistorique(agent_id)
   }
 }
